bbe47bdbb786c4941ca50aa3f96e517dc41822e4
Chsherbina <as_la_n@mail.ru>
2024-10-12 17:48:32 +0600

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
